6090154967a13d31cb3fbcbbc00be92711e9ec74
Shucheng Yang <ysc@shao.ac.cn>
2019-12-26 23:09:29 +0800

    it seems work

--- cPP/week3/week3p4.cpp
+++ cPP/week3/week3p4.cpp
@@ -38,18 +38,21 @@ private:
     string name;  
     int t;                     // time
     int m;                     // total strength
-    int num;                   // warrior number    
+    int num;                   // warrior number 
+    int numCir;                 // created warrior number   
     int strengthList[5];       // dragon 、ninja、iceman、lion、wolf 
+    bool OutputFinished;               
     warrior warriorList[5];    // dragon 、ninja、iceman、lion、wolf 
 
 public:
     headquater(string name_, int m_, const int * strengthList_)
     {
         name = name_;
+        t  = 0;   
         m = m_;
         num = 0; 
-        t  = 0;   
-
+        numCir = 0;
+        OutputFinished = false;
         for (int i = 0; i < 5; i++)
         {
             strengthList[i] = strengthList_[i];
@@ -72,8 +75,6 @@ public:
                 {
                     warriorList[createOrder[i]].addWarrior();  // add Warrior
                     m -= warriorList[createOrder[i]].strength; //m decrease
-                    
-
                     cout << setfill('0') << setw(3) << t++ << ' ' << name << warriorList[createOrder[i]].wname \
                     << ++num << " born with strength " << warriorList[createOrder[i]].strength << ','\
                     << warriorList[createOrder[i]].wnum << ' ' << warriorList[createOrder[i]].wname \
@@ -95,10 +96,49 @@ public:
             {
                 flag[i] = false;
             }
-            
         }
+        return 0;
     }
 
+
+    bool createWarriorT(const int * createOrder)
+    {
+        int count = 0;
+        for (; count < 5; numCir++)
+        {
+            numCirAdd(numCir);
+            // cout << i << endl;
+            if (m - warriorList[createOrder[numCir]].strength > 0)
+            {
+                warriorList[createOrder[numCir]].addWarrior();  // add Warrior
+                m -= warriorList[createOrder[numCir]].strength; //m decrease
+                cout << setfill('0') << setw(3) << t++ << ' ' << name << warriorList[createOrder[numCir]].wname \
+                << ++num << " born with strength " << warriorList[createOrder[numCir]].strength << ','\
+                << warriorList[createOrder[numCir]].wnum << ' ' << warriorList[createOrder[numCir]].wname \
+                << "in " << name << "headquater" << endl;
+                numCir++;
+                return true;
+            }
+            count++;
+        }
+
+        if(OutputFinished == false)
+        {
+            cout << setfill('0') << setw(3) << t << ' ' << name << "headquarter stops making warriors" << endl;
+            OutputFinished = true;
+        }
+        return false;                
+    }
+
+    int numCirAdd(int & number)
+    {
+        if(number > 4)
+        {
+            number -= 5;
+        }
+        return 0;
+    }
+    
 
     int outputM()
     {
@@ -114,11 +154,7 @@ public:
         } 
         cout << endl;
     }
-
-
-
 };
-
 
 int main()  
 {
@@ -127,6 +163,7 @@ int main()
     int strengthList[5];    // dragon 、ninja、iceman、lion、wolf 
     int redOrder[5] = {2, 3, 4, 1, 0};
     int blueOrder[5] = {3, 0, 1, 2, 4};
+    bool a,b;
     cin >> n;
 
     for (int i = 0; i < n; i++)
@@ -142,13 +179,19 @@ int main()
 
         cout << "Case:" << i + 1 << endl;
 
-        red.createWarrior(redOrder);
-        blue.createWarrior(blueOrder);
+        // red.createWarrior(redOrder);
+        // blue.createWarrior(blueOrder);
 
-        // blue.outputSL();
-        // blue.outputM();
-    
-
+        for (int i = 0;; i++)
+        {
+           a = !red.createWarriorT(redOrder);
+           b = !blue.createWarriorT(blueOrder);
+           
+           if (a && b)
+           {
+               break;
+           }
+        }
     }
     return 0;
 }
