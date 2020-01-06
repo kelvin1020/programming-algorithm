cb1978f20ae0491669349ed42901ce03c5d964c2
Shucheng Yang <ysc@shao.ac.cn>
2020-01-07 02:13:56 +0800

    pointer problem

--- cPP/week5/week5p2.cpp
+++ cPP/week5/week5p2.cpp
@@ -7,7 +7,34 @@ using namespace std;
 // 0 dragon 、1 ninja、2 iceman、3 lion、4 wolf
 
 
-class headquarter;
+class warrior;
+
+class headquarter
+{
+private:
+    string name;  
+    int t;                     // time
+    int m;                     // total strength
+    int num;                   // warrior number 
+    int numCir;                 // created warrior number   
+    bool OutputFinished; 
+
+    int warriorNum[5] = {0};
+    const char* warriorNameList[5] = {"dragon", "ninja", "iceman", "lion", "wolf"}; 
+    warrior *warriorList[10000] = {NULL};    // dragon 、ninja、iceman、lion、wolf 
+
+    friend class dragon;
+    friend class lion;    
+
+public:
+
+    int strengthList[5];       // dragon 、ninja、iceman、lion、wolf   
+    headquarter(string name_, int m_, const int * strengthList_);
+    int addWarrior(int code, int num, int wstrength); // 0 dragon 、1 ninja、2 iceman、3 lion、4 wolf
+    bool createWarrior(const int * createOrder);
+    ~headquarter();
+};
+
 
 class warrior
 {
@@ -19,7 +46,12 @@ protected:
     const headquarter * wbase;  //从属基地
  
 public:
+    const char * weaponName[3] = {"sword", "bomb", "arrow"};
     warrior(int num, int strength, const headquarter *base):wnum(num), wstrength(strength), wbase(base){};  //{ cout<< wstrength << ' ' << wnum;};
+    int say()
+    {
+        return 0;
+    };
 };
 
 
@@ -33,7 +65,12 @@ public:
     dragon(int num, int strength, const headquarter *base):warrior(num, strength, base)
     {
         weapon = num%3;
-        // morale = ((double) ((*wbase).m)) / ((double) ((*wbase).strengthList[0]));
+        morale = ((double) (wbase->m)) / ((double) (wbase->strengthList[0]));
+    }
+    int say()
+    {
+        cout << "It has a " << weaponName[weapon] << ",and it's morale is " << setw(3) << morale << endl;
+        return 0;
     }
 };
 
@@ -72,112 +109,93 @@ private:
 public:
     lion(int num, int strength, const headquarter *base):warrior(num, strength, base)
     {
-        // loyalty = (wbase->m);
+        loyalty = (wbase->m);
     }
 };
-
 
 class wolf:public warrior
 {
 public:
-    wolf(int num, int strength, const headquarter *base):warrior(num, strength, base){};    
+    wolf(int num, int strength, const headquarter *base):warrior(num, strength, base){};
 };
 
-class headquarter
+
+headquarter::headquarter(string name_, int m_, const int * strengthList_)
 {
-private:
-    string name;  
-    int t;                     // time
-
-    int num;                   // warrior number 
-    int numCir;                 // created warrior number   
-    bool OutputFinished; 
-
-    int warriorNum[5] = {0};
-    const char* warriorNameList[5] = {"dragon", "ninja", "iceman", "lion", "wolf"}; 
-    warrior *warriorList[10000] = {NULL};    // dragon 、ninja、iceman、lion、wolf 
-
-    // friend warrior;
-
-public:
-    int m;                     // total strength
-    int strengthList[5];       // dragon 、ninja、iceman、lion、wolf   
-
-    headquarter(string name_, int m_, const int * strengthList_)
+    name = name_;
+    t  = 0;   
+    m = m_;
+    num = 0; 
+    numCir = 0;
+    OutputFinished = false;
+    for (int i = 0; i < 5; i++)
     {
-        name = name_;
-        t  = 0;   
-        m = m_;
-        num = 0; 
-        numCir = 0;
-        OutputFinished = false;
-        for (int i = 0; i < 5; i++)
+        strengthList[i] = strengthList_[i];
+    }
+}
+
+
+ int headquarter::addWarrior(int code, int num, int wstrength) // 0 dragon 、1 ninja、2 iceman、3 lion、4 wolf
+{
+
+    switch (code)
+    {
+    case 0: warriorList[num] = new dragon(num, wstrength, this); break;
+    case 1: warriorList[num] = new ninja(num, wstrength, this); break;
+    case 2: warriorList[num] = new iceman(num, wstrength, this); break;
+    case 3: warriorList[num] = new lion(num, wstrength, this); break;
+    case 4: warriorList[num] = new wolf(num, wstrength, this); break;        
+    }
+    return 0;
+}
+
+bool headquarter::createWarrior(const int * createOrder)
+{
+    int count = 0;
+    int wstrength;
+    const char* wname;
+
+    for (; count < 5; numCir++)
+    {
+        numCir %= 5;
+        wstrength = strengthList[createOrder[numCir]];
+        wname = warriorNameList[createOrder[numCir]];
+
+        if (m - wstrength >= 0)  //判断是否还能创建
         {
-            strengthList[i] = strengthList_[i];
+            addWarrior(createOrder[numCir],  num, wstrength);          // add Warrior
+            m -= strengthList[createOrder[numCir]];                    //m decrease
+
+            cout << setfill('0') << setw(3) << t++ << ' ' << name << ' ' << wname << ' '\
+            << ++num << " born with strength " << wstrength << ','\
+            << ++warriorNum[createOrder[numCir]] << ' ' << wname \
+            << " in " << name << ' '<< "headquarter" << endl;
+
+            warriorList[num-1]->say();   //say
+
+            numCir++;
+            return true;
         }
+        count++;
+
     }
 
-
-    int addWarrior(int code, int num, int wstrength) // 0 dragon 、1 ninja、2 iceman、3 lion、4 wolf
+    if(OutputFinished == false)
     {
-
-        switch (code)
-        {
-        case 0: warriorList[num] = new dragon(num, wstrength, this); break;
-        case 1: warriorList[num] = new ninja(num, wstrength, this); break;
-        case 2: warriorList[num] = new iceman(num, wstrength, this); break;
-        case 3: warriorList[num] = new lion(num, wstrength, this); break;
-        case 4: warriorList[num] = new wolf(num, wstrength, this); break;        
-        }
-        return 0;
+        cout << setfill('0') << setw(3) << t << ' ' << name << ' ' << "headquarter stops making warriors" << endl;
+        OutputFinished = true;
     }
+    return false;         
+}
 
-    bool createWarrior(const int * createOrder)
+headquarter::~headquarter()
+{
+    for (int i = 0; warriorList[i] != NULL; i++)
     {
-        int count = 0;
-        int wstrength;
-        const char* wname;
-
-        for (; count < 5; numCir++)
-        {
-            numCir %= 5;
-            wstrength = strengthList[createOrder[numCir]];
-            wname = warriorNameList[createOrder[numCir]];
-
-            if (m - wstrength >= 0)  //判断是否还能创建
-            {
-                addWarrior(createOrder[numCir],  num, wstrength);          // add Warrior
-                m -= strengthList[createOrder[numCir]];                    //m decrease
-
-                cout << setfill('0') << setw(3) << t++ << ' ' << name << ' ' << wname \
-                << ++num << " born with strength " << wstrength << ','\
-                << ++warriorNum[createOrder[numCir]] << ' ' << wname \
-                << "in " << name << "headquarter" << endl;
-
-                numCir++;
-                return true;
-            }
-            count++;
-
-        }
-
-        if(OutputFinished == false)
-        {
-            cout << setfill('0') << setw(3) << t << ' ' << name << "headquarter stops making warriors" << endl;
-            OutputFinished = true;
-        }
-        return false;         
+        delete  warriorList[i];
+        warriorList[i] = NULL;
     }
-
-    ~headquarter()
-    {
-        for (int i = 0; warriorList[i] != NULL; i++)
-        {
-            delete  warriorList[i];
-            warriorList[i] = NULL;
-        }
-    };
-};
+}
 
 int main()  
 {
